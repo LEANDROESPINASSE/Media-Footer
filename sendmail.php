dfcc1ffd533f112af15fc4bad50f65498524ea44
Rodrigo Artero <99847246+Rodri3752@users.noreply.github.com>
2023-02-28 14:50:17 -0300

    cambios 1
    ...

--- sendmail.php
+++ sendmail.php
@@ -4,7 +4,7 @@
     $email_to =   'support@email.com'; //the address to which the email will be sent
     $name     =   $_POST['name'];  
     $email    =   $_POST['email'];
-    $subject  =   $_POST['subject'];
+    $phone  =   $_POST['phone'];
     $message  =   $_POST['message'];
     
     /*the $header variable is for the additional headers in the mail function,
@@ -14,7 +14,7 @@
     $headers  = "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
     
-    if(mail($email_to, $subject, $message, $headers)){
+    if(mail($email_to, $phone, $message, $headers)){
         echo 'sent'; // we are sending this text to the ajax request telling it that the mail is sent..      
     }else{
         echo 'failed';// ... or this one to tell it that it wasn't sent    
